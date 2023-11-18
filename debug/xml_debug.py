777b106ee01e888bfa0eb3c65e05ebcea38cfa06
mayumiPc <mayumiobara089@gmail.com>
2023-11-18 20:33:53 +0900

    処理開始と終了がわかるようにした

--- debug/xml_debug.py
+++ debug/xml_debug.py
@@ -1,12 +1,14 @@
 from urllib import request
 
 def debug():
+    print("started")
     with open("result.txt", "w", encoding="utf-8") as f:
         url = "https://radiko.jp/v3/station/region/full.xml"
         r = request.Request(url)
         with request.urlopen(r) as res:
             xml_text = res.read().decode()
             f.write(xml_text)
+            print("done")
 
 if __name__ == "__main__":
     debug()
