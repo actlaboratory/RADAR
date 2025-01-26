ab3703ffbc36ae058ec0f7bfd35b242821b650af
mayumiPc <mayumiobara089@gmail.com>
2025-01-26 09:42:04 +0900

    不要なプリント分を削除

--- views/main.py
+++ views/main.py
@@ -580,6 +580,5 @@ class Events(BaseEvents):
 	def recording_schedule(self, event):
 		rw = recordingWizzard.RecordingWizzard(self.selected, self.parent.stid[self.selected])
 		rw.init()
-		rw.getFileType(self.parent.app.config.getint("recording", "menu_id"))
 		rw.Show()
 		return
